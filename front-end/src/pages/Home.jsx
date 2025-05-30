7e3704247adec1b16b49a19f649762dfe5f2fe29
gabri3939 <gabrieliaralian1@gmail.com>
2025-05-30 00:56:28 -0300

    30/0/2025 100%

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -4,7 +4,8 @@ import Item from '../components/Item'
 const Home = () => {
   return (
     <section>
-   <div className=" gap-8 grid grid-cols-[repeat(auto-fit,minmax(255px,1fr))]  p-8 max-w-7xl mx-auto ">
+ <div className="gap-8 grid grid-cols-[repeat(auto-fit,minmax(255px,1fr))] p-8 max-w-7xl mx-auto">
+
   <Item/>
   <Item/>
   <Item/>
